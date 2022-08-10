bac16e3e2927cb30aee7155f59ef88f7a8ff1cb3
canslercp <canslercp@gmail.com>
2022-08-10 08:53:38 -0400

    forgot to save homeRoutes...

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -53,16 +53,16 @@ router.get('/post/:id', async (req, res) => {
 router.get('/dashboard', /*withAuth,*/ async (req, res) => {
     try{
         //find the logged in user based on the session ID
-        // const userData = await User.findByPk(req.session.user_id, {
-        //     attributes: { exclude: ['password']},
-        //     include: [{ model: Post }],
-        // });
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password']},
+            include: [{ model: Post }],
+        });
 
-        // const user = userData.get({ plain: true });
+        const user = userData.get({ plain: true });
 
         res.render('dashboard', {
-            // ...user,
-            // logged_in: true
+            ...user,
+            logged_in: true
         });
     } catch (err) {
         res.status(500).json(err);
